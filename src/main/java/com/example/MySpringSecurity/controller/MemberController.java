418ece26ecf7ee464d436dac3ce3bb145d18126d
LydiaHsieh <hch34242@gmail.com>
2024-09-17 17:29:37 +0800

    feat: add login api

--- src/main/java/com/example/MySpringSecurity/controller/MemberController.java
+++ src/main/java/com/example/MySpringSecurity/controller/MemberController.java
@@ -4,10 +4,18 @@ import com.example.MySpringSecurity.entity.Member;
 import com.example.MySpringSecurity.service.MemberService;
 import com.example.MySpringSecurity.vo.MemberVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class MemberController {
@@ -20,12 +28,34 @@ public class MemberController {
 
 
     @PostMapping("/register")
-    public String register(@RequestBody MemberVo vo) {
+    public ResponseEntity<Map<String, Object>> register(@RequestBody MemberVo vo) {
         String hashedPassword = passwordEncoder.encode(vo.getPassword());
         vo.setPassword(hashedPassword);
 
         Member member = memberService.create(vo);
 
-        return "註冊成功";
+        Map<String, Object> map = new HashMap<>();
+        map.put("isSuccess", true);
+        return ResponseEntity.ok(map);
+    }
+
+    //"/login"已被Spring Security使用
+    @PostMapping("/userLogin")
+    public ResponseEntity<Map<String, Object>> login(Authentication authentication) {
+        //帳號密碼驗證交由Spring Security處理，能執行到這裡表示登入成功
+
+        Map<String, Object> map = new HashMap<>();
+
+        //取得帳號
+        String name = authentication.getName();
+
+        //取得權限
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        map.put("isSuccess", true);
+        map.put("userName", name);
+        map.put("authorities", authorities);
+
+        return ResponseEntity.ok(map);
     }
 }
--- src/main/java/com/example/MySpringSecurity/security/SecurityConfig.java
+++ src/main/java/com/example/MySpringSecurity/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,12 +22,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                //設定Session的創建機制 (Spring Security 6.X後要添加這一段才會去創建Session和Cookie)
+                //登入後就可以透過Cookie的JSessionID通過需要認證的API
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
 
                         .requestMatchers("/register").permitAll()
+                        .requestMatchers("/userLogin").authenticated()
 
                         .anyRequest().authenticated())
 
